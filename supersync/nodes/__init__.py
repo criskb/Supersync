ab145e8496649701cedef7ebcf7eedc767de5d62
Cris K Bjørndal <82761237+criskb@users.noreply.github.com>
2026-03-10 14:24:09 +0100

    Add CombinePoseData stream merger with filtering and diagnostics

--- supersync/nodes/__init__.py
+++ supersync/nodes/__init__.py
@@ -1,3 +1,9 @@
+from .combine_pose_data import CombinePoseData, CombinePoseDataConfig
 from .eye_motion_synth import EyeMotionSynth, EyeMotionSynthConfig
 
-__all__ = ["EyeMotionSynth", "EyeMotionSynthConfig"]
+__all__ = [
+    "CombinePoseData",
+    "CombinePoseDataConfig",
+    "EyeMotionSynth",
+    "EyeMotionSynthConfig",
+]
